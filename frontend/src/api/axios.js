9d5d395c8e895daae3476d22ba3c40212176700b
Prateek Parihar <prateekparihar999@gmail.com>
2025-12-10 15:18:09 +0530

    Updated backend and frontend configuration

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,13 @@
+// frontend/src/api/axios.js  (CRA)
 import axios from 'axios';
 
+// const DEFAULT_LOCAL = 'http://localhost:5000';
+const baseURL = "https://fliper-project-4.onrender.com" || 'http://localhost:5000';
+
 const axiosInstance = axios.create({
-  baseURL: "https://fliper-project-4.onrender.com",
+  baseURL,
   withCredentials: true,
+  timeout: 10000,
 });
 
 export default axiosInstance;
